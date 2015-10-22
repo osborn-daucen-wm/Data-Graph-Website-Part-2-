f9339656b0d0183b17d7e08fe64d6ba1682332de
daucen <daucen.osborn@west-mec.org>
2015-10-22 15:01:53 -0700

    This is all of the working files. I had to refork the repository, so this is my first commit.

---
+++ photo_scripts.js
@@ -0,0 +1,152 @@
+function box1() {
+    if (document.getElementById("pic1").src == "http://www.rikvin.com/wp-content/uploads/2013/12/singapore-flag1.jpg")
+    {
+        document.getElementById("pic1").src = "http://ecx.images-amazon.com/images/I/51Wv92l7PGL._SY300_.jpg";
+    }
+    else {
+        document.getElementById("pic1").src = "http://www.rikvin.com/wp-content/uploads/2013/12/singapore-flag1.jpg";
+    }
+}
+
+function box2() {
+    if (document.getElementById("pic2").src == "http://media.fx-mm.com/wp-content/uploads/Flag-of-Malaysia.jpg")
+    {
+        document.getElementById("pic2").src = "https://lh3.ggpht.com/yq635jt6HHCDSsavtLkATwbOrduYAPgeF8hZrebpDGebcL-_odF5LNLslbXBaOEF2-A=w300";
+    }
+    else {
+        document.getElementById("pic2").src = "http://media.fx-mm.com/wp-content/uploads/Flag-of-Malaysia.jpg";
+    }
+}
+
+function box3() {
+    if (document.getElementById("pic3").src == "http://www.africalegalnetwork.com/wp-content/uploads/2012/10/china-flag-300x300.jpg")
+    {
+        document.getElementById("pic3").src = "http://fsa.zedge.net/scale.php?img=OS81LzgvNC8xLTkzODkwOTQtOTU4NDYxNy5qcGc&ctype=1&v=4&q=71&xs=300&ys=300&sig=d040304800cea076b2c3460260bb014ac2f1c850";
+    }
+    else {
+        document.getElementById("pic3").src = "http://www.africalegalnetwork.com/wp-content/uploads/2012/10/china-flag-300x300.jpg";
+    }
+}
+
+function box4() {
+    if (document.getElementById("pic4").src == "http://freewallpaperdownloads.net/wp-content/uploads/2015/03/ipad-wallpaper-uk-flag-300x300.jpg")
+    {
+        document.getElementById("pic4").src = "http://fsb.zedge.net/scale.php?img=NC8wLzMvMi8xLTkzODkxODQtNDAzMjcwMi5qcGc&ctype=1&v=4&q=71&xs=300&ys=300&sig=32a1df0e8d4c4d64954fc731d80dcea26836ef02";
+    }
+    else {
+        document.getElementById("pic4").src = "http://freewallpaperdownloads.net/wp-content/uploads/2015/03/ipad-wallpaper-uk-flag-300x300.jpg";
+    }
+}
+
+function box5() {
+    if (document.getElementById("pic5").src == "http://insights.ingredientsnetwork.com/wp-content/uploads/2015/01/japan-flag-9184105-300x300.jpg")
+    {
+        document.getElementById("pic5").src = "https://lh6.ggpht.com/gsF9AKNj71oLOb3hF0-rXWryukSWF6jquF8AxAo7o_jJJMc0WEUsT3ivh3FDX3mi_Q=w300";
+    }
+    else {
+        document.getElementById("pic5").src = "http://insights.ingredientsnetwork.com/wp-content/uploads/2015/01/japan-flag-9184105-300x300.jpg";
+    }
+}
+
+function box6() {
+    if (document.getElementById("pic6").src == "http://media.fx-mm.com/wp-content/uploads/Australian-flag.jpg")
+    {
+        document.getElementById("pic6").src = "https://lh3.ggpht.com/2MUDjHOncgDTuLn8hYrky4_AMMENGe9Fyl62Y-2Wk7_ehg_lGoeN9EQBOWDe4g8l0Y8=w300";
+    }
+    else {
+        document.getElementById("pic6").src = "http://media.fx-mm.com/wp-content/uploads/Australian-flag.jpg";
+    }
+}
+
+function box7() {
+    if (document.getElementById("pic7").src == "https://lh5.ggpht.com/u4WS8-tZLDyV645OTZT-t5uD5RvmTU20iPXELTfDiJjgtdvpoZZk0u143dL6Kv6MaNLU=w300")
+    {
+        document.getElementById("pic7").src = "https://lh4.ggpht.com/Dtq2hfe-JZGnm2J3_oABwLOrSdfZqZ5oeGo_8EyyhUCrJ9kAjyBbVwt5SuSGCuNhQIk=w300";
+    }
+    else {
+        document.getElementById("pic7").src = "https://lh5.ggpht.com/u4WS8-tZLDyV645OTZT-t5uD5RvmTU20iPXELTfDiJjgtdvpoZZk0u143dL6Kv6MaNLU=w300";
+    }
+}
+
+function box8() {
+    if (document.getElementById("pic8").src == "https://wallpaperscraft.com/image/flag_symbols_colors_materials_stripes_color_indonesia_44096_300x300.jpg")
+    {
+        document.getElementById("pic8").src = "https://lh5.ggpht.com/XioC4pHk_650Os81h3NeEDntFRJFiHFwjbncBOb0gVj15FCrznc4EX85L7xd_Kimu20=w300";
+    }
+    else {
+        document.getElementById("pic8").src = "https://wallpaperscraft.com/image/flag_symbols_colors_materials_stripes_color_indonesia_44096_300x300.jpg";
+    }
+}
+
+function box9() {
+    if (document.getElementById("pic9").src == "https://lh6.ggpht.com/LG-CjIUpta8_y4hvvzup1xFaH4RnTQOLE_S7ZpYvIIAfJj-HD5SoO-74JDpU_lF9LHg=w300")
+    {
+        document.getElementById("pic9").src = "http://androidsandwich.com/app_thumbnails/3d-flag-pakistan-lwp-thumbnail.png";
+    }
+    else {
+        document.getElementById("pic9").src = "https://lh6.ggpht.com/LG-CjIUpta8_y4hvvzup1xFaH4RnTQOLE_S7ZpYvIIAfJj-HD5SoO-74JDpU_lF9LHg=w300";
+    }
+}
+
+function changeDes1() {
+    var des1 = function (answer) {
+        $('#des1').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes2() {
+    var des1 = function (answer) {
+        $('#des2').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes3() {
+    var des1 = function (answer) {
+        $('#des3').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes4() {
+    var des1 = function (answer) {
+        $('#des4').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes5() {
+    var des1 = function (answer) {
+        $('#des5').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes6() {
+    var des1 = function (answer) {
+        $('#des6').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes7() {
+    var des1 = function (answer) {
+        $('#des7').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes8() {
+    var des1 = function (answer) {
+        $('#des8').html(answer);
+    };
+    des1(prompt());
+}
+
+function changeDes9() {
+    var des1 = function (answer) {
+        $('#des9').html(answer);
+    };
+    des1(prompt());
+}
